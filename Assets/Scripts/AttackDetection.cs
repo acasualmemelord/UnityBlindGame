69eea9c7c68a3ec6ec07ea0b2e07e65c86a5d5c7
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2024-05-02 22:39:20 -0500

    the

--- Assets/Scripts/AttackDetection.cs
+++ Assets/Scripts/AttackDetection.cs
@@ -11,18 +11,26 @@ public class AttackDetection : MonoBehaviour {
         float scale = enemyStats.stats[StatNames.AttackRadius];
         sphere.transform.localScale = new Vector3(scale, scale, scale);
     }
+    private void OnTriggerEnter(Collider other) {
+        canHit = true;
+    }
 
     private void OnTriggerStay(Collider c) {
         if (c.name == "First Person Player" && canHit) {
+            Debug.Log(sphere.GetInstanceID() + " entered");
             StartCoroutine(Waiter());
         }
     }
     private IEnumerator Waiter() {
         canHit = false;
+        Debug.Log(enemyStats.stats[StatNames.Attack] - playerStats.stats[StatNames.Defense]);
         playerStats.Damage(enemyStats.stats[StatNames.Attack] - playerStats.stats[StatNames.Defense]);
         yield return new WaitForSeconds(enemyStats.stats[StatNames.AttackInterval]);
         canHit = true;
     }
 
-    private void OnTriggerExit(Collider c) { canHit = false; }
+    private void OnTriggerExit(Collider c) {
+        //Debug.Log("left");
+        canHit = false; 
+    }
 }
