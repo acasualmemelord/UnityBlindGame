427aff04064f10b28cdb20d41469b4a3cc734340
Zovrah <46391669+Zovrah@users.noreply.github.com>
2024-05-26 22:58:41 -0700

    Update Winpoint.cs

--- Assets/Scripts/Environment/Winpoint.cs
+++ Assets/Scripts/Environment/Winpoint.cs
@@ -6,6 +6,9 @@ public class Winpoint : MonoBehaviour {
     private void OnTriggerEnter(Collider c) {
         if (c.CompareTag("Player")) {
             GameManager.LoadWinScene();
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
         }
     }
 }
