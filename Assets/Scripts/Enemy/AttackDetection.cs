dabd7960e12be8d71b764c8e98c68a976cb35b4f
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2024-05-29 00:16:06 -0700

    L

--- Assets/Scripts/Enemy/AttackDetection.cs
+++ Assets/Scripts/Enemy/AttackDetection.cs
@@ -5,28 +5,36 @@ using UnityEngine;
 public class AttackDetection : MonoBehaviour {
     public EnemyStats enemyStats;
     public PlayerStats playerStats;
-    public SphereCollider sphere;
-    public static bool canHit = true;
+    private float radius;
+    public bool canHit = true;
     public Animate animate;
+    public LayerMask playerMask;
+    private Collider[] colliders = new Collider[1];
+    private bool entered = false;
 
     private void Start() {
-        float scale = enemyStats.stats[StatNames.AttackRadius];
-        sphere.radius = scale;
+        radius = enemyStats.stats[StatNames.AttackRadius];
     }
-    private void OnTriggerEnter(Collider c) {
-        if (c.CompareTag("Environment") || c.CompareTag("Enemy")) return;
-        if (c.transform.parent != null && c.transform.parent.CompareTag("Enemy")) return;
-        if (c.CompareTag("Player")) {
-            canHit = true;
+
+    private void Update() {
+        colliders = new Collider[1];
+        if (Physics.OverlapSphereNonAlloc(transform.position, radius, colliders, playerMask) > 0) {
+            if (!entered) {
+                entered = true;
+                canHit = true;
+            }
+            if (canHit) {
+                animate.Attack();
+                StartCoroutine(Waiter());
+            }
+        }
+        else {
+            if (!animate.GetComponent<Animator>().GetBool("isDying")) animate.Reset();
+            entered = false;
+            canHit = false;
         }
     }
 
-    private void OnTriggerStay(Collider c) {
-        if (c.CompareTag("Player") && canHit) {
-            animate.Attack();
-            StartCoroutine(Waiter());
-        }
-    }
     private IEnumerator Waiter() {
         canHit = false;
         playerStats.Damage(enemyStats.stats[StatNames.Attack] - playerStats.stats[StatNames.Defense]);
@@ -34,8 +42,26 @@ public class AttackDetection : MonoBehaviour {
         canHit = true;
     }
 
+    /*
+    private void OnTriggerEnter(Collider c) {
+        if (c.CompareTag("Environment") || c.CompareTag("Enemy")) return;
+        if (c.transform.parent != null && c.transform.parent.CompareTag("Enemy")) return;
+        if (c.CompareTag("Player")) {
+            Debug.Log("hi 2");
+            canHit = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider c) {
+        if (c.CompareTag("Player") && canHit) {
+            Debug.Log("hi");
+            animate.Attack();
+            StartCoroutine(Waiter());
+        }
+    }
+    
     private void OnTriggerExit(Collider c) {
         if (!animate.GetComponent<Animator>().GetBool("isDying")) animate.Reset();
         canHit = false; 
-    }
+    }*/
 }
